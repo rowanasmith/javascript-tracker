baa26d9c359d5dacd401f5bdd491aebd165fc3eb
Rowan Smith <theintheredwoods@gmail.com>
2019-09-24 13:57:21 -0500

    Table header

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tracking Javascript 30 Day Challenge</h1>
       </header>
+      <table>
+        <tr>
+          <th>Day</th>
+          <th>Project Name</th>
+          <th>Description</th>
+          <th>Github Repo</th>
+        </tr>
+      </table>
     </div>
   );
 }
