5f8854edace6cc161e43f0d402c8cb8705a8018e
Rowan Smith <theintheredwoods@gmail.com>
2019-10-01 14:13:22 -0500

    working on troubleshooting get

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('get list items');
+    console.log('Get list items');
     pool.query(`SELECT "projects"."day" as "project_day", 
     "projects"."name" as "project_name", 
     "projects"."description" as "project_description",
